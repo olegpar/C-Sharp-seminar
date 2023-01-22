7b01dd063c816c884ad7275bf6d25c4110994d54
Oleg Parfenyuk <olegpar@yandex.ru>
2023-01-22 13:32:53 +0300

    task 42

---
+++ task 40/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 40: Напишите программу, которая принимает на
+// вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+void Treug(int A, int B, int C)
+{
+    if(A + B > C && A + C > B && C + B > A)
+    {
+        Console.WriteLine("Это треугольник ");
+    }
+    else
+    {
+        Console.WriteLine("Это НЕ трегугольник ");
+    }
+}
+Console.WriteLine("Введите длину первой стороны");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину второй стороны");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину третьей стороны");
+int C = Convert.ToInt32(Console.ReadLine());
+Treug(A, B, C);
---
+++ task 42/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void GetBinaryView(int number)
+{
+if(number == 0) return;
+GetBinaryView(number/2);
+Console.Write(number%2);
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+GetBinaryView(number);
